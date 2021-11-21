63de4a92b784227f259ea861f0def69ce022514e
TechnikTim <77175275+TechnikTimDEV@users.noreply.github.com>
2021-11-21 19:30:31 +0100

    Config Valid-Checker + Robots-Settings + Navbar-Setting

--- src/main/kotlin/de/dertev/Application.kt
+++ src/main/kotlin/de/dertev/Application.kt
@@ -8,11 +8,13 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.io.File
 
+val configFile = FileManager(File("config.json"), Json.encodeToString(hashMapOf("port" to "80", "host" to "localhost", "navbar" to true, "robots-index" to true, "robots-follow" to true)))
+
 fun main() {
 
     //Creates important files
     FileManager(File("files/")).createDir()
-    FileManager(File("navbar.json"), Json.encodeToString(hashMapOf( "md-server" to "https://github.com/dertev/md-server/", "Home" to "/"))).createFile()
+    FileManager(File("navbar.json"), Json.encodeToString(hashMapOf("md-server" to "https://github.com/dertev/md-server/", "Home" to "/"))).createFile()
     FileManager(File("404.html"), "404").createFile()
     FileManager(File("style.css"), exampleCSS).createFile()
     FileManager(
@@ -21,10 +23,16 @@ fun main() {
     ).createFile()
 
     //Load Config
-    val configFile = FileManager(File("config.json"), Json.encodeToString(hashMapOf("port" to "80", "host" to "localhost")))
     configFile.createFile()
     val settings: HashMap<String, String> = Json.decodeFromString(configFile.file.readText())
 
+    //Check if the config is valide
+    if ("port" in settings && "host" in settings && "navbar" in settings && "robots-index" in settings && "robots-follow" in settings) {
+        println("Config is valide!")
+    } else {
+        println("Config is invalid!")
+        return
+    }
 
     //Starts the Ktor server with routing.
     embeddedServer(Netty, port = settings["port"]!!.toInt(), host = settings["host"]!!) {
--- src/main/kotlin/de/dertev/FileManager.kt
+++ src/main/kotlin/de/dertev/FileManager.kt
@@ -12,23 +12,47 @@ class FileManager(val file: File, private val defaultContent: String="") {
 
     //Loads HTML from Markdown
     fun md2HTML(): String {
+
+        val settings: HashMap<String, String> = Json.decodeFromString(configFile.file.readText())
+
         val src = file.readText()
         val flavour = CommonMarkFlavourDescriptor()
         val parsedTree = MarkdownParser(flavour).buildMarkdownTreeFromString(src)
         val html = HtmlGenerator(src, parsedTree, flavour).generateHtml().replaceFirst("<body>", "")
 
-        val navBarData: HashMap<String, String> = Json.decodeFromString(File("navbar.json").readText())
-
         var navbar = "<p class=\"navbar\">"
-        navBarData.forEach {
-            navbar += "<a href=\"${it.value}\">${it.key}</a> "
-        }
-        navbar += "</p>"
 
+        if (settings["navbar"].toBoolean()) {
+            val navBarData: HashMap<String, String> = Json.decodeFromString(File("navbar.json").readText())
+
+            navBarData.forEach {
+                navbar += "<a href=\"${it.value}\">${it.key}</a> "
+            }
+            navbar += "</p>"
+        }
         var title = file.name.split("|")
         title = listOf(title[title.size-1])
 
-        return "<!DOCTYPE html><html><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><title>${title}</title><link href=\"/style.css\", rel=\"stylesheet\"></head><body>$navbar$html</html>"
+        var robotsIndex = ""
+        var robotsFollow = ""
+
+        robotsIndex = if (settings["robots-index"].toBoolean()) {
+            "index"
+        } else {
+            "noindex"
+        }
+
+        robotsFollow = if (settings["robots-follow"].toBoolean()) {
+            "follow"
+        } else {
+            "nofollow"
+        }
+
+        if (settings["navbar"].toBoolean()) {
+            return "<!DOCTYPE html><html><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><meta name=\"robots\" content=\"$robotsIndex,$robotsFollow\"><title>${title}</title><link href=\"/style.css\", rel=\"stylesheet\"></head><body>$navbar$html</html>"
+        } else {
+            return "<!DOCTYPE html><html><head><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><meta name=\"robots\" content=\"$robotsIndex,$robotsFollow\"><title>${title}</title><link href=\"/style.css\", rel=\"stylesheet\"></head><body>$html</html>"
+        }
     }
 
     //Creates file, if this doesn't exist
