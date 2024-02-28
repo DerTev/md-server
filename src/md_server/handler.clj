c6c7f4d6fed287708c83e03d34e13f292036fe01
Tim <tim@dertev.de>
2024-02-28 17:10:01 +0100

    Add sitemap

--- src/md_server/handler.clj
+++ src/md_server/handler.clj
@@ -1,13 +1,15 @@
 (ns md-server.handler
   (:require [md-server.markdown :as md]
-            [clojure.string :as cstr]))
+            [clojure.string :as cstr]
+            [md-server.sitemap :as sitemap])
+  (:import java.io.File))
 
 (defn handle [req]
   (let [uri (:uri req)]
     (condp contains? uri
       #{"/style.css"} {:body   (slurp "https://cdn.jsdelivr.net/npm/water.css@2/out/water.css")
                        :status 200}
-      #{"/sitemap" "/sitemap/"} {:body   "Sitemap coming soon..."
+      #{"/sitemap" "/sitemap/"} {:body   (sitemap/render (File. "content/"))
                                  :status 404}
       (or (md/render-content (str uri ".md"))
           (md/render-content (str uri "/index.md"))
