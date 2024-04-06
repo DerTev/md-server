abf733a5be144268b4fcc4fc9c290de69e88c254
Tim <tim@dertev.de>
2024-04-06 19:03:26 +0200

    Release 2.0.0

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject md-server "2.0.0-SNAPSHOT"
+(defproject md-server "2.0.0"
   :description "Serve markdown files with ease."
   :url "https://dertev.de/md-server/"
   :license {:name "MIT License"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.11.2"]
                  [http-kit "2.7.0"]
                  [lynxeyes/dotenv "1.1.0"]
                  [hiccup "2.0.0-RC3"]
                  [com.kiranshila/cybermonday "0.6.215"]
-                 [ring/ring-core "1.11.0"]]
+                 [ring/ring-core "1.12.1"]]
   :main ^:skip-aot md-server.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 # md-server
 
 *Serve markdown files with ease.*
+
+## Installation
+
+1. Download the jar from the releases
+2. Run it
+3. Place your content into the ``content``-Folder
+4. Done!
+
+**We recommend to place md-server behind a [reverse proxy](https://www.cloudflare.com/learning/cdn/glossary/reverse-proxy/)!**
